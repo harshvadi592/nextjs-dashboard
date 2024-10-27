8cdbe96573cbf4e435e6fb7779c931842779f094
harshvadi07 <harshvadi12345@gmail.com>
2024-10-27 19:06:19 +0530

    lints

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,6 +63,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
+      error,
     };
   }
 
@@ -77,7 +78,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
   } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    return { message: "Database Error: Failed to Delete Invoice.", error };
   }
 }
 
@@ -109,7 +110,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
   } catch (error) {
-    return { message: "Database Error: Failed to Update Invoice." };
+    return { message: "Database Error: Failed to Update Invoice.", error };
   }
 
   revalidatePath("/dashboard/invoices");
